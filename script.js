c2e15072a769df152cd572bb130d73ddd8866a9c
Nick Strayer <nstrayer@uvm.edu>
2015-07-04 22:46:26 -0700

    Stuffs working well. Code commented

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-var height = 850,
-    width = 500,
+//just some nonsense to move objects to front easily.
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function(){
+    this.parentNode.appendChild(this);
+  });
+};
+
+var height = 500,
+    width = 800,
     padding = 10;
 
 var svg = d3.select("#viz").append("svg")
     .attr("height", height)
     .attr("width", width)
 
+//A counter variable to keep track of where we are in the visualization.
 var count = 0
 
 d3.csv("stateData.csv", function(data){
@@ -20,9 +28,14 @@ d3.csv("stateData.csv", function(data){
         d.percAg = +d.percAg
     })
 
+    //We want to have the steps equaly spaced across the screen
     var x = d3.scale.ordinal()
-        .domain(d3.range(data.length))
+        .domain(d3.range(3))
 		.rangeRoundBands([0,width],0.2);
+
+    //Set up scaling functions for each of the displayed statistics.
+    //There is probably a more efficient way of doing this, but given that
+    //both scales are called at the same time in the lines I couldn't think of one.
 
     var yNumMarkets = d3.scale.linear()
         .domain([0, d3.max(data, function(d){return d.numMarkets})])
@@ -30,7 +43,7 @@ d3.csv("stateData.csv", function(data){
 
     var yPopPerMarket = d3.scale.linear()
         .domain([0, d3.max(data, function(d){return d.popPerMarket})])
-        .range([height - padding, padding])
+        .range([padding, height - padding])
 
     var yPercAg = d3.scale.linear()
         .domain([0, d3.max(data, function(d){return d.percAg})])
@@ -40,33 +53,29 @@ d3.csv("stateData.csv", function(data){
         .data(data)
         .enter()
         .append("circle")
-        .attr("cx", function(d,i){return padding})
+        .attr("class", function(d){return d.abrev})
+        .attr("cx", function(d,i){return x(count)})
         .attr("cy", function(d){return yNumMarkets(d.numMarkets)})
         .attr("r", 5)
         .attr("fill", "#2ca25f")
-        .on("click", function(d){
-            // d3.select(this).attr("r", 10)
-            changePosition()
-            // animatelines("step0")
-        })
-        // .on("mouseout", function(d){d3.select(this).attr("r", 5)})
+        .on("click", function(d){ changePosition() })
 
+    //Start drawing the trend lines.
+    //This is broken up into different steps because in order to animate the lines,
+    //they must already be drawn. This also allows us to attach data to them.
     svg.selectAll(".step0")
         .data(data)
         .enter()
         .append("line")
-        // .attr("id", function(d,i){return "line" + i})
         .attr("id", function(d,i){return "step0_line" + i})
         .attr("class", function(d,i) {return "line step0 " + d.abrev })
-        .attr("x1", padding )     // x position of the first end of the line
+        .attr("x1", x(0) )     // x position of the first end of the line
         .attr("y1", function(d){return yNumMarkets(d.numMarkets)})      // y position of the first end of the line
-        .attr("x2", padding + 200)     // x position of the second end of the line
+        .attr("x2", x(1))     // x position of the second end of the line
         .attr("y2", function(d){return yPopPerMarket(d.popPerMarket)})    // y position of the second end of the line
         .attr("stroke", "#99d8c9")
-        // .attr("stroke-opacity", 0.5)
         .attr("stroke-width", 1)
         .style("opacity",0);
-
 
     svg.selectAll(".step1")
         .data(data)
@@ -74,23 +83,25 @@ d3.csv("stateData.csv", function(data){
         .append("line")
         .attr("id", function(d,i){return "step1_line" + i})
         .attr("class", function(d,i) {return "line step1 " + d.abrev })
-        .attr("x1", padding + 200)     // x position of the first end of the line
+        .attr("x1", x(1))     // x position of the first end of the line
         .attr("y1", function(d){return yPopPerMarket(d.popPerMarket)})      // y position of the first end of the line
-        .attr("x2", padding + 200*2)     // x position of the second end of the line
+        .attr("x2", x(2))     // x position of the second end of the line
         .attr("y2", function(d){return yPercAg(d.percAg)})    // y position of the second end of the line
         .attr("stroke", "#99d8c9")
-        // .attr("stroke-opacity", 0.5)
         .attr("stroke-width", 1)
         .style("opacity",0);
 
 
-    // console.table(data)
+    // console.table(data) //A nice way to checkout out the data.
 
     //I put my functions down here.
 
+    //Move the circle and animate the lines.
     var changePosition = function() {
 
+        //start line animation.
         animatelines("step" + count)
+
         //use this to determine which scale to use.
         order = ["numMarkets", "popPerMarket", "percAg"]
         var key = order[count + 1]
@@ -114,36 +125,45 @@ d3.csv("stateData.csv", function(data){
             .transition()
             .duration(2500)
             .ease("linear")
-            // .delayNumMarkets(function(d,i){return i*50})
             .attr("cy", function(d){return scale(d[key])})
-            .attr("cx", function(d,i){return padding + 200*(count+1)})
+            .attr("cx", function(d,i){return x(count+1)})
 
+        //increment the counter up one to indicate we are onto the next step.
         count = count + 1
     }
-
 })
 
 function animatelines(step) {
-
     d3.selectAll("." + step).style("opacity","0.5");
 
     //Select All of the lines and process them one by one
     d3.selectAll("." + step).each(function(d,i){
 
+    //for some reason .getTotalLength works for paths, but not plain lines.
+    //Busting out some pythagorean theorem to get it done though.
     var yd = d3.select(this).attr("y2") - d3.select(this).attr("y1"),
         xd = d3.select(this).attr("x2") - d3.select(this).attr("x1"),
         totalLength = Math.sqrt(xd*xd + yd*yd);
-
-    console.log(totalLength)
 
 	d3.select("#" + step + "_" + "line" + i).attr("stroke-dasharray", totalLength + " " + totalLength)
 	  .attr("stroke-dashoffset", totalLength)
 	  .transition()
 	  .duration(2500)
-	  .ease("linear") //Try linear, quad, bounce... see other examples here - http://bl.ocks.org/hunzy/9929724
+	  .ease("linear")
 	  .attr("stroke-dashoffset", 0)
-	//   .style("stroke-width",3)
-
     })
+}
+
+function highlight(state, step){
+    //select Point
+    d3.select("circle." + state)
+        .moveToFront()
+        .attr("fill", "red")
+
+    //select Line
+    d3.selectAll("line." + step + "." + state)
+        .moveToFront()
+        .attr("stroke-width", 3)
+        .attr("stroke", "red")
 
 }
